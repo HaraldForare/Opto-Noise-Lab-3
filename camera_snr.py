6e869d0dde0078d5dd24c58e72b3d1253c1d2d15
HaraldForare <harald.forare@gmail.com>
2025-03-09 17:20:48 +0100

    Read noise limit added

--- camera_snr.py
+++ camera_snr.py
@@ -8,19 +8,22 @@ import numpy as np
 
 exposure_time = loadmat("data/camera_snr/exposure_time.mat")["StatsTime"].flatten()
 signal = loadmat("data/camera_snr/mean_count.mat")["StatsMean"].flatten()
-error_std_dev = loadmat("data/camera_snr/std_dev.mat")["StatsStd"].flatten()
+noise = loadmat("data/camera_snr/std_dev.mat")["StatsStd"].flatten()
 
 
-snr = signal / error_std_dev
+snr = signal / noise
 
 
-assert exposure_time.shape == signal.shape == error_std_dev.shape
+assert exposure_time.shape == signal.shape == noise.shape
 
 
 
 signal_pred = np.geomspace(np.min(signal), np.max(signal), 1000)
-shot_noise_pred = np.sqrt(signal_pred)
+snr_shot_noise_pred = np.sqrt(signal_pred)
 
+
+read_noise = 5
+snr_read_noise_pred = signal_pred / read_noise
 
 
 
@@ -31,13 +34,13 @@ if __name__ == "__main__":
 
     plt.figure("SNR_vs_signal")
     plt.loglog(signal, snr, marker="x", label="Camera performance")
-    plt.loglog(signal_pred, shot_noise_pred, linestyle=":", label="Shot noise limit")
+    plt.loglog(signal_pred, snr_shot_noise_pred, linestyle=":", label="Shot noise limit")
+    plt.loglog(signal_pred, snr_read_noise_pred, linestyle=":", label="Read noise limit")
 
     plt.xlabel("Signal [counts]")
     plt.ylabel("SNR [1]")
     plt.grid(True)
     plt.legend()
-
 
     plt.show()
 
